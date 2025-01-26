30926bd0de384b3a43b0fff2575f01bb8482bf0c
KKMAI <khotchakorn61@gmail.com>
2025-01-27 03:24:06 +0700

    Add Cat

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,96 +1,89 @@
 "use client"
-import "antd/dist/reset.css"
-import { Form, Input, Button, Checkbox } from "antd";
 import axios from 'axios';
-import '@ant-design/v5-patch-for-react-19';
+import Cat from "./script";
+import './style.css'
+
 
 interface FormValues {
-  username?: string;
-  password?: string;
-  remember?: boolean;
-}
+    username: string;
+    password: string;
+    remember: boolean;
+  }
 
-export default function Login() {
-  const onFinish = (values: FormValues) => {
-    const {username, password} = values
-    axios.post('http://localhost:3001/validatePassword', {username, password})
-    .then(res => {
-      if(res.data.validation){
-        alert('Your password is correct, thank you for your service')
-      }
-      else{
-        alert('Your password is not correct. Please try again.')
-      }
-    })
-    console.log('Received values of form: ', values);
-  };
-
-//   const onFinishFailed = (errorInfo) => {
-//     console.log("Failed:", errorInfo);
-//   };
-
-  return (
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8
-      }}
-      wrapperCol={{
-        span: 16
-      }}
-      initialValues={{
-        remember: true
-      }}
-      onFinish={onFinish}
-    //   onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!"
+export default function Login(){
+    const onFinish = (values: FormValues) => {
+        const {username, password} = values
+        axios.post('http://localhost:3001/validatePassword', {username, password})
+        .then(res => {
+          if(res.data.validation){
+            alert('Your password is correct, thank you for your service')
           }
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!"
+          else{
+            alert('Your password is not correct. Please try again.')
           }
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+        })
+        console.log('Received values of form: ', values);
+      };
+    // Configuration for the Cat component
+    const catConfig = {
+      leftId: "leftSide",
+      handWrapSelector: ".hand-wrap .hand",
+      mouthSelector: ".mouth",
+    };
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  );
-}
+    return(
+      <div>
+        {/* ทางซ้าย */}
+        <Cat {...catConfig} />
+        <div id="leftSide" className="container h-screen w-3/5 left-0">
+                <div className="cat">
+                    <div className="cat-head">
+                        <div className="cat-ears">
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div className="cat-face">
+                            <div className="cat-eyes">
+                                <div></div>
+                                <div></div>
+                            </div>
+                            <div className="cat-nose">
+                                <div className="mustache">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div className="mustache">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <span className="mouth hide"></span>
+                        </div>
+                    </div>
+                    <div className="hand-wrap">
+                        <div className="hand">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        {/* ทางขวา */}
+        <div className='h-screen w-2/5 bg-amber-200 absolute insert-y right-0 flex justify-center items-center'>
+            <form className='w-1/2'>
+                <div className="border-b-1 border-black">
+                  <input className="w-full bg-white" type="text" placeholder='username'/>
+                </div>
+                <div className="border-b-1 border-black">
+                  <input className="w-full bg-white" type="text" placeholder='password'/>
+                </div>
+              </form>
+        </div>
+        
+      </div>
+      
+    )
+  }
