18720af0b22478081674df9635011e9441ae6225
kasomsri <bess11234@hotmail.com>
2025-02-06 20:35:38 +0700

    MERGE

--- app/(guest)/find-house/error.tsx
+++ app/(guest)/find-house/error.tsx
@@ -12,7 +12,7 @@ export default function Error({
 }) {
   useEffect(() => {
     // Optionally log the error to an error reporting service
-    console.error(error);
+    // console.error(error);
   }, [error]);
  
   return (
