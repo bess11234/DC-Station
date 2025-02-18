406134bf55e00f516d6337316455020c40c00c32
kasomsri <bess11234@hotmail.com>
2025-02-19 00:21:28 +0700

    UPDATE Animal Editdata

--- app/components/(manager)/animals/EditingData.tsx
+++ app/components/(manager)/animals/EditingData.tsx
@@ -45,7 +45,7 @@ export function EditingData({ animal }: { animal: Animal }) {
     }))
 
     // Input Illness
-    const [inputIllness, setInputIllness] = useState<displayIllness[]>(animal.healthHistories.illeness != undefined && animal.healthHistories.illeness.map((v, i) => {
+    const [inputIllness, setInputIllness] = useState<displayIllness[]>(inputAnimal.healthHistories.illeness != undefined && inputAnimal.healthHistories.illeness.map((v, i) => {
         return {
             id: i,
             name: v.name,
@@ -55,8 +55,9 @@ export function EditingData({ animal }: { animal: Animal }) {
         }
     }) || [])
 
-    // Input Personality
+    // Input Personality | Illness
     const inputPersonality = useRef<HTMLInputElement>(null)
+    const inputIllnessHTML = useRef<HTMLInputElement>(null)
 
     // Form Animal
     const inputForm = useRef<HTMLFormElement>(null)
@@ -70,37 +71,49 @@ export function EditingData({ animal }: { animal: Animal }) {
         setInputAnimal(prevState => ({ ...prevState, [key]: value }))
     }, 300)
 
-    const handlehealthHistories = useDebouncedCallback((value: string | undefined, key: string) => {
+    const handlehealthHistories = useDebouncedCallback((value: string | Illness[] | undefined, key: string) => {
         if (value == undefined) value = ""
-        setInputAnimal(prevState => ({ ...prevState, ["handlehealthHistories"]: { [key]: value } }))
+
+        const healthTemp = {...inputAnimal.healthHistories}
+        if (key == "spayingStatus") {
+            healthTemp.spayingStatus = value == "1"
+        }
+
+        if (key == "illeness" && typeof value != "string") {
+            healthTemp.illeness = value
+        }
+
+        setInputAnimal(prevState => ({ ...prevState, ["healthHistories"]: healthTemp }))
     }, 300)
 
-    const handleInputIllness = useDebouncedCallback((value: string | undefined, id: number = inputIllness.length) => {
-        let temp: display[] = []
+    const handleInputIllness = useDebouncedCallback((name: string | undefined, status: "Under treatment" | "Recovered" | "Chronic" | "Under surveillance", id: number = inputIllness.length) => {
+        let temp: displayIllness[] = []
 
-        if (value == undefined) value = ""
-        value = value.trim()
+        if (name == undefined) name = ""
+        name = name.trim()
 
-        if (id < displayPersonalities.length) {
-            temp = displayPersonalities.map((v) => {
+        if (id < inputIllness.length) {
+            temp = inputIllness.map((v) => {
                 if (v.id == id) {
-                    // ถ้ามีค่าส่งมาจะปรับ Value
-                    if (value) v.value = value
+                    // ถ้ามีค่าส่งมาจะปรับ name
+                    if (name) { v.value = name; v.name = name }
                     // ถ้าไม่มีค่าส่งมาปรับ Visible
-                    else if ((value == "" || value == undefined)) v.visible = false
+                    else if ((name == "" || name == undefined)) v.visible = false
+
+                    v.status = status
                 }
                 return v
             })
-            setDisplayPersonalities(temp)
-        } else if (value != "") {
-            temp = [...displayPersonalities, { id: id, value: value, visible: true }]
-            setDisplayPersonalities(temp)
+            setInputIllness(temp)
+        } else if (name != "") {
+            temp = [...inputIllness, { id: id, value: name, name: name, status: "Under treatment", visible: true }]
+            setInputIllness(temp)
         }
 
         // อัพเดทข้อมูล Personalities จาก displayPersonalities
-        if (temp) { setInputAnimal(prevState => ({ ...prevState, ["personalities"]: temp.filter(v => v.visible).map(v => v.value) })) }
-        if (inputPersonality.current && "value" in inputPersonality.current) {
-            inputPersonality.current.value = ""
+        if (temp) { handlehealthHistories(temp.filter(v => v.visible), "illeness") }
+        if (inputIllnessHTML.current && "value" in inputIllnessHTML.current) {
+            inputIllnessHTML.current.value = ""
         }
     }, 300)
 
@@ -172,6 +185,22 @@ export function EditingData({ animal }: { animal: Animal }) {
                 value: v
             }
         }))
+        setInputIllness(animal.healthHistories.illeness != undefined && animal.healthHistories.illeness.map((v, i) => {
+            return {
+                id: i,
+                name: v.name,
+                value: v.name,
+                status: v.status,
+                visible: true,
+            }
+        }) || [])
+    }
+
+    function validateIllness(status: string): "Under treatment" | "Recovered" | "Chronic" | "Under surveillance" {
+        if (status == "Under treatment" || status == "Recovered" || status == "Chronic" || status == "Under surveillance") {
+            return status
+        }
+        return "Under treatment"
     }
 
     return (
@@ -240,54 +269,67 @@ export function EditingData({ animal }: { animal: Animal }) {
                 <textarea className="p-3 field-sizing-content rounded-xl input-focus-theme" onChange={(e) => handleInput(e.target.value, "history")} name="history" id="animalHistory" defaultValue={animal.history} />
 
                 {/* ประวัติสุขภาพ */}
-                <p className="text-2xl">ประวัติสุขภาพ</p>
+                <div className="outline outline-dashed p-6! mt-6 rounded-xl hover:outline-theme-400 focus-within:outline-theme-400">
+                    <p className="text-2xl">ประวัติสุขภาพ</p>
 
-                {/* สถานะการทำหมั่น */}
-                <label htmlFor="animalSpayingStatus" className="text-lg p-0!">สถานะการทำหมั่น</label>
-                <select className="*:bg-white *:dark:bg-black2 py-1! px-3 mb-3 w-full rounded-xl input-focus-theme" onChange={(e) => handlehealthHistories(e.target.value, "spayingStatus")} name="spayingStatus" id="animalSpayingStatus" defaultValue={animal.healthHistories.spayingStatus ? "1" : "0"} required>
-                    <option value="0">ยังไม่ทำหมัน</option>
-                    <option value="1">ทำหมั่นแล้ว</option>
-                </select>
+                    {/* สถานะการทำหมั่น */}
+                    <label htmlFor="animalSpayingStatus" className="text-lg p-0!">สถานะการทำหมั่น</label>
+                    <select className="*:bg-white *:dark:bg-black2 py-1! px-3 mb-3 w-full rounded-xl input-focus-theme" onChange={(e) => handlehealthHistories(e.target.value, "spayingStatus")} name="spayingStatus" id="animalSpayingStatus" defaultValue={animal.healthHistories.spayingStatus ? "1" : "0"} required>
+                        <option value="0">ยังไม่ทำหมัน</option>
+                        <option value="1">ทำหมั่นแล้ว</option>
+                    </select>
 
-                {/* อาการเจ็บป่วย */}
-                <p className="text-lg p-0!">อาการเจ็บป่วย</p>
-                <AnimatePresence mode="popLayout">
-                    {/* แสดงอาการเจ็บป่วย */}
-                    {displayPersonalities.filter(v => v.visible).map((v, i) => (
-                        (<motion.div layout initial={{ opacity: 0, y: -10, transition: { ease: "easeIn", delay: 2 } }} animate={{ opacity: 1, y: 0, scale: 1 }} exit={{ opacity: 0, scale: 0, y: -30, transition: { ease: "easeOut", duration: 0.3, delay: 0 } }} className="flex flex-row space-x-3 items-center mb-3 p-0!" key={v.id} >
-                            <XCircleIcon className="cursor-pointer size-6 hover:opacity-40 active:opacity-60 rounded-full" onClick={() => handlePersonalitiesInput("", v.id)} /><input className="py-1 px-3 w-full rounded-xl input-focus-theme" onBlur={(e) => handlePersonalitiesInput(e.target.value, v.id)} type="text" id={`illness_${v.id}`} defaultValue={v.value} />
+                    {/* อาการเจ็บป่วย */}
+                    <p className="text-lg p-0!">อาการเจ็บป่วย</p>
+                    <AnimatePresence mode="popLayout">
+                        {/* แสดงอาการเจ็บป่วย */}
+                        {inputIllness.filter(v => v.visible).map((v) => (
+                            (<motion.div layout initial={{ opacity: 0, y: -10, transition: { ease: "easeIn", delay: 2 } }} animate={{ opacity: 1, y: 0, scale: 1 }} exit={{ opacity: 0, scale: 0, y: -30, transition: { ease: "easeOut", duration: 0.3, delay: 0 } }} className="flex flex-row space-x-3 items-center mb-3 p-0!" key={v.id} >
+                                <XCircleIcon className="cursor-pointer size-6 hover:opacity-40 active:opacity-60 rounded-full" onClick={() => handleInputIllness("", "Under treatment", v.id)} />
+                                <div className="grid grid-cols-3 w-full">
+                                    <input className="col-span-2 py-1 px-3 w-full rounded-l-xl border-r-0 input-focus-theme" onChange={(e) => handleInputIllness(e.target.value, "Under treatment", v.id)} type="text" id={`illness_name${v.id}`} defaultValue={v.value} />
+                                    <select className="*:bg-white *:dark:bg-black2 py-1 px-3 w-full rounded-r-xl input-focus-theme" onChange={(e) => handleInputIllness(v.name, validateIllness(e.target.value), v.id)} id={`illness_status${v.id}`} defaultValue={v.status}>
+                                        <option value="Under treatment">กำลังรักษา</option>
+                                        <option value="Recovered">รักษาหายแล้ว</option>
+                                        <option value="Chronic">เรื้อรัง</option>
+                                        <option value="Under surveillance">เฝ้าระวัง</option>
+                                    </select>
+                                </div>
+                            </motion.div>
+                            )
+                        ))}
+
+                        {/* ช่องกรอกอาการป่วยเพิ่มเติม */}
+                        <motion.div layout className="p-0! flex mt-1" >
+                            <input ref={inputIllnessHTML} className="p-3 w-full rounded-l-xl border-r-0 input-focus-theme" type="text" name="newIllness" id="newIllness" placeholder="เพิ่มข้อมูลอาการป่วย เช่น โรคพยาธิหนอนหัวใจ, โรคฉี่หนู" defaultValue="" onKeyDown={(e) => e.key == "Enter" && handleInputIllness(e.currentTarget.value, "Under treatment")} />
+                            <button onClick={(e) => handleInputIllness(e.currentTarget.parentNode?.querySelectorAll("input")[0].value, "Under treatment")} className="border border-black2 dark:border-white text-theme-600 dark:text-theme-400 text-nowrap rounded-r-xl px-3 cursor-pointer font-semibold outline-offset-4" type="button">
+                                <span className="flex gap-x-1">เพิ่มข้อมูล<ArrowTurnDownLeftIcon className="size-6" /></span>
+                            </button>
                         </motion.div>
-                        )
-                    ))}
-
-                    {/* ช่องกรอกอาการป่วยเพิ่มเติม */}
-                    <motion.div layout className="p-0! flex mt-1" >
-                        <input ref={inputPersonality} className="p-3 w-full rounded-l-xl border-r-0 input-focus-theme" type="text" name="newPersonality" id="newPersonality" placeholder="เพิ่มข้อมูลอุปนิสัย" defaultValue="" onKeyDown={(e) => e.key == "Enter" && handlePersonalitiesInput(e.currentTarget.value)} />
-                        <button onClick={(e) => handlePersonalitiesInput(e.currentTarget.parentNode?.querySelectorAll("input")[0].value)} className="border border-black2 dark:border-white text-theme-600 dark:text-theme-400 text-nowrap rounded-r-xl px-3 cursor-pointer font-semibold outline-offset-4" type="button">
-                            <span className="flex gap-x-1">เพิ่มข้อมูล<ArrowTurnDownLeftIcon className="size-6" /></span>
-                        </button>
-                    </motion.div>
-                </AnimatePresence>
+                    </AnimatePresence>
+                </div>
 
                 {/* อุปนิสัย */}
-                <p className="text-2xl">อุปนิสัย</p>
-                <AnimatePresence mode="popLayout">
-                    {/* ข้อมูลนิสัย */}
-                    {displayPersonalities.filter(v => v.visible).map((v, i) => (
-                        (<motion.div layout initial={{ opacity: 0, y: -10, transition: { ease: "easeIn", delay: 2 } }} animate={{ opacity: 1, y: 0, scale: 1 }} exit={{ opacity: 0, scale: 0, y: -30, transition: { ease: "easeOut", duration: 0.3, delay: 0 } }} className="flex flex-row space-x-3 items-center mb-3 p-0!" key={v.id} >
-                            <XCircleIcon className="cursor-pointer size-6 hover:opacity-40 active:opacity-60 rounded-full" onClick={() => handlePersonalitiesInput("", v.id)} /><input className="py-1 px-3 w-full rounded-xl input-focus-theme" onBlur={(e) => handlePersonalitiesInput(e.target.value, v.id)} type="text" id={`personality_${v.id}`} defaultValue={v.value} />
-                        </motion.div>
-                        )
-                    ))}
+                <div className="outline outline-dashed p-6! mt-6 rounded-xl hover:outline-theme-400 focus-within:outline-theme-400">
+                    <p className="text-2xl">อุปนิสัย</p>
+                    <AnimatePresence mode="popLayout">
+                        {/* ข้อมูลนิสัย */}
+                        {displayPersonalities.filter(v => v.visible).map((v) => (
+                            (<motion.div layout initial={{ opacity: 0, y: -10, transition: { ease: "easeIn", delay: 2 } }} animate={{ opacity: 1, y: 0, scale: 1 }} exit={{ opacity: 0, scale: 0, y: -30, transition: { ease: "easeOut", duration: 0.3, delay: 0 } }} className="flex flex-row space-x-3 items-center mb-3 p-0!" key={v.id} >
+                                <button><XCircleIcon className="cursor-pointer size-6 hover:opacity-40 active:opacity-60 rounded-full" onClick={() => handlePersonalitiesInput("", v.id)} /></button><input className="py-1 px-3 w-full rounded-xl input-focus-theme" onBlur={(e) => handlePersonalitiesInput(e.target.value, v.id)} type="text" id={`personality_${v.id}`} defaultValue={v.value} />
+                            </motion.div>
+                            )
+                        ))}
 
-                    {/* ช่องกรอกนิสัยเพิ่มเติม */}
-                    <motion.div layout className="p-0! flex mt-1" >
-                        <input ref={inputPersonality} className="p-3 w-full rounded-l-xl border-r-0 input-focus-theme" type="text" name="newPersonality" id="newPersonality" placeholder="เพิ่มข้อมูลอุปนิสัย" defaultValue="" onKeyDown={(e) => e.key == "Enter" && handlePersonalitiesInput(e.currentTarget.value)} />
-                        <button onClick={(e) => handlePersonalitiesInput(e.currentTarget.parentNode?.querySelectorAll("input")[0].value)} className="border border-black2 dark:border-white text-theme-600 dark:text-theme-400 text-nowrap rounded-r-xl px-3 cursor-pointer font-semibold outline-offset-4" type="button">
-                            <span className="flex gap-x-1">เพิ่มข้อมูล<ArrowTurnDownLeftIcon className="size-6" /></span>
-                        </button>
-                    </motion.div>
-                </AnimatePresence>
+                        {/* ช่องกรอกนิสัยเพิ่มเติม */}
+                        <motion.div layout className="p-0! flex mt-1" >
+                            <input ref={inputPersonality} className="p-3 w-full rounded-l-xl border-r-0 input-focus-theme" type="text" name="newPersonality" id="newPersonality" placeholder="เพิ่มข้อมูลอุปนิสัย" defaultValue="" onKeyDown={(e) => e.key == "Enter" && handlePersonalitiesInput(e.currentTarget.value)} />
+                            <button onClick={(e) => handlePersonalitiesInput(e.currentTarget.parentNode?.querySelectorAll("input")[0].value)} className="border border-black2 dark:border-white text-theme-600 dark:text-theme-400 text-nowrap rounded-r-xl px-3 cursor-pointer font-semibold outline-offset-4" type="button">
+                                <span className="flex gap-x-1">เพิ่มข้อมูล<ArrowTurnDownLeftIcon className="size-6" /></span>
+                            </button>
+                        </motion.div>
+                    </AnimatePresence>
+                </div>
 
                 {/* Other Images */}
                 <>
